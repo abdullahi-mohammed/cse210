a7dd3b908eab1c9479d512e282992a4a02bf6950
abdullahi <moh.abdullahi2003@gmail.com>
2024-11-08 17:59:43 +0100

    completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
+// A >= 90
+// B >= 80
+// C >= 70
+// D >= 60
+// F < 60
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Grade percentage?");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+        {
+            Console.Write("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.Write("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.Write("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.Write("D");
+        }
+        else
+        {
+            Console.Write("F");
+        }
     }
 }

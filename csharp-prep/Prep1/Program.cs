33e3600622a96f39bce7aac1d62c1f93bd505cc4
abdullahi <moh.abdullahi2003@gmail.com>
2024-11-08 17:50:28 +0100

    completed prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
 class Program
+
+// What is your first name? Scott
+// What is your last name? Burton
+
+// Your name is Burton, Scott Burton.
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }

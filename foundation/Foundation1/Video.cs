1a34ad860f8d2776a5bdc64b5c219c8cf24caf12
abdullahi <moh.abdullahi2003@gmail.com>
2024-12-01 19:29:40 +0100

    done

--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -5,28 +5,28 @@ public class Video
     public string Title { get; set; }
     public string Author { get; set; }
     public int Length { get; set; }
-    private List<Comment> Comments { get; set; }
+    private List<Comment> _Comments { get; set; }
 
     public Video(string title, string author, int length)
     {
         Title = title;
         Author = author;
         Length = length;
-        Comments = new List<Comment>();
+        _Comments = new List<Comment>();
     }
 
     public void AddComment(Comment comment)
     {
-        Comments.Add(comment);
+        _Comments.Add(comment);
     }
 
     public int GetCommentCount()
     {
-        return Comments.Count;
+        return _Comments.Count;
     }
 
     public List<Comment> GetComments()
     {
-        return Comments;
+        return _Comments;
     }
 }
